d57227a2bf93a53d5787f4c6fb5e6528c8fba319
Renzo De Nardi <renzodenardi@gmail.com>
2012-02-17 15:51:18 +0000

    towards wind comparison against simulink

--- tests/testWind.m
+++ tests/testWind.m
@@ -12,146 +12,174 @@ plots = 1;
 cd('wind');
 
 %%% model parameters
+runSim();
 
-Vfts = 1; hft = 5; W20ft = 1; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 10; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 10; hft = 5; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 10; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 30; hft = 100; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 10; hft = 500; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 10; hft = 10; W20ft = 1; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 10; hft = 10; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 10; hft = 10; W20ft = 100; phi = 0; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-% %%% wind direction
-
-Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=pi/4;
-e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_45dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=pi/2;
-e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_90dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=3*pi/4;
-e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_135dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
- 
-Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=pi;
-e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_180dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
- 
- Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=9*pi/8;
- e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_202dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
- 
- Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=7*pi/4;
- e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_315dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
- 
- Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=15*pi/8;
- e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_337dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-%%% orientations
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = pi/8; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p0t22p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = pi/4; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p0t45p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 3*pi/8; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p0t67p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = pi/2; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p0t90p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/8; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p22t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p45t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 3*pi/8; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p67t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/2; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p90t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-
-Vfts = 100; hft = 50; W20ft = 10; phi = pi/8; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_22p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = pi/4; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_67p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 3*pi/8; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_67p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = pi/2; theta = 0; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_90p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-
-Vfts = 100; hft = 50; W20ft = 10; phi = pi/4; theta = 0; psi = 3*pi/8; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_45p0t67p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = pi/8; theta =  pi/4; psi = 0; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_22p45t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/4; psi = pi/4; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_0p45t45p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
-
-Vfts = 100; hft = 50; W20ft = 10; phi = 3*pi/8; theta = pi/4; psi = pi/8; dir=0;
-e = e | runAndCompare('windlog_100V50h10w20_67p45t22p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots); 
+% Vfts = 1; hft = 5; W20ft = 1; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 10; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 10; hft = 5; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 10; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 30; hft = 100; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 10; hft = 500; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 10; hft = 10; W20ft = 1; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 10; hft = 10; W20ft = 10; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 10; hft = 10; W20ft = 100; phi = 0; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('theoretical',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% %% wind direction
+% 
+% Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=pi/4;
+% e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_45dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=pi/2;
+% e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_90dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=3*pi/4;
+% e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_135dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=pi;
+% e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_180dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=9*pi/8;
+% e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_202dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=7*pi/4;
+% e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_315dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 20; hft = 20; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=15*pi/8;
+% e = e | runAndCompare('windlog_20V20h10w20_0p45t0p_337dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% % orientations
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = pi/8; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p0t22p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = pi/4; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p0t45p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = 3*pi/8; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p0t67p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 0; psi = pi/2; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p0t90p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/8; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p22t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/4; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p45t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = 3*pi/8; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p67t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/2; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p90t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = pi/8; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_22p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = pi/4; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_67p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 3*pi/8; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_67p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = pi/2; theta = 0; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_90p0t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = pi/4; theta = 0; psi = 3*pi/8; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_45p0t67p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = pi/8; theta =  pi/4; psi = 0; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_22p45t0p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 0; theta = pi/4; psi = pi/4; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_0p45t45p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
+% 
+% Vfts = 100; hft = 50; W20ft = 10; phi = 3*pi/8; theta = pi/4; psi = pi/8; dir=0;
+% e = e | runAndCompare('windlog_100V50h10w20_67p45t22p_0dir.mat',T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots);
 
 cd('..');
 
 end
 
-% function e = runSim()
-% eX=zeros(23,N);
-% X=zeros(13,N);
-% 
-% % a control that in absence of noise gives perfect hover
-% U = [0;0;0.59004353928;0;11];
-% 
-% % new state structure
-% global state;
-% 
-% % create simulator object
-% qrsim = QRSim();
-% 
-% % load task parameters and do housekeeping
-% qrsim.init(task);
-% 
-% 
-% for i=1:N    
-%     % step simulator
-%     qrsim.step(U);
-%           
-%     eX(1:20,i) = state.platforms(1).getEX();
-%     eX(21:23,i) = state.platforms(1).getA();
-%     X(:,i) = state.platforms(1).getX();
-%     
-% 
-% end
-% 
-% end 
+function e = runSim()
+
+N = 10000;
+
+e = 0;
+
+% trick to pass stuff to simulink, surely there mut be a better way
+evalin('base','global state;');
+
+% new state structure
+global state;
+
+state.i =0;
+
+% only needed if using the pid controller
+clear global pid;
+
+state.simin=zeros(N,6);
+
+% create simulator object
+qrsim = QRSim();
+
+% load task parameters and do housekeeping
+qrsim.init('TaskCompareWindWithSimulink');
+
+wps=[   0,   0,  -10, 0;
+        0,   0,  -35, 0;
+       35,   0,  -10, 0;
+      -35,   0,  -10, 0;
+        0,  35,  -10, 0;
+        0, -35,  -10, 0];
+    
+j =1;
+for i=1:N, 
+
+    % compute controls
+    U=quadrotorPID(state.platforms(1).getX(),wps(j,:));
+   
+    % step simulator
+    qrsim.step(U);   
+    
+    if(norm(state.platforms(1).getX(1:3)-wps(j,1:3)')<0.5)
+       disp('switching'); 
+       j=j+1;
+    end
+    
+    if(j>6), j=1; end
+end
+
+evalin('base','simin=state.simin;');
+
+simOut = sim('wind_and_turb_comparison','SaveState','off');
+        
+yout = simOut.get('yout');       
+
+end
 
 function e = runAndCompare(reference,T,W20ft,Vfts,dir,hft,phi,theta,psi,N,TOL,plots)
 
@@ -227,7 +255,7 @@ else
     Pww = Pww.*0.25;
 end
 
-if(plots)    
+if(plots)
     figure;
     subplot(1,3,1);
     semilogx(f,10*log10(Puu));
@@ -249,19 +277,19 @@ if(plots)
     semilogx(f,10*log10(Pmww),'r');
     grid on;
     axis([0.01 100 -80 20]);
-        
-%     figure;
-%     subplot(1,3,1);
-%     semilogx(f,10*log10((Pmuu))-10*log10(Puu),'r');
-%     grid on;
-%     
-%     subplot(1,3,2);
-%     semilogx(f,10*log10((Pmvv))-10*log10(Pvv),'r');
-%     grid on;
-%     
-%     subplot(1,3,3);
-%     semilogx(f,10*log10((Pmww))-10*log10(Pww),'r');
-%     grid on;
+    
+    %     figure;
+    %     subplot(1,3,1);
+    %     semilogx(f,10*log10((Pmuu))-10*log10(Puu),'r');
+    %     grid on;
+    %
+    %     subplot(1,3,2);
+    %     semilogx(f,10*log10((Pmvv))-10*log10(Pvv),'r');
+    %     grid on;
+    %
+    %     subplot(1,3,3);
+    %     semilogx(f,10*log10((Pmww))-10*log10(Pww),'r');
+    %     grid on;
     
 end
 
---
+++ tests/wind/PelicanForWindTesting.m
@@ -0,0 +1,119 @@
+classdef PelicanForWindTesting<Pelican
+    % Class that extend Pelican to save wind data useful for wind testing
+    %
+    % Pelican Properties:
+    % X   - state = [px;py;pz;phi;theta;psi;u;v;w;p;q;r;thrust]
+    %       px,py,pz         [m]     position (NED coordinates)
+    %       phi,theta,psi    [rad]   attitude in Euler angles right-hand ZYX convention
+    %       u,v,w            [m/s]   velocity in body coordinates
+    %       p,q,r            [rad/s] rotational velocity  in body coordinates
+    %       thrust           [N]     rotors thrust
+    %
+    % eX  - estimated state = [~px;~py;~pz;~phi;~theta;~psi;0;0;0;~p;~q;~r;0;~ax;~ay;~az;
+    %                          ~h;~pxdot;~pydot;~hdot]
+    %       ~px,~py,~pz      [m]     position estimated by GPS (NED coordinates)
+    %       ~phi,~theta,~psi [rad]   estimated attitude in Euler angles right-hand ZYX convention
+    %       0,0,0                    placeholder (the uav does not provide velocity estimation)
+    %       ~p,~q,~r         [rad/s] measured rotational velocity in body coordinates
+    %       0                        placeholder (the uav does not provide thrust estimation)
+    %       ~ax,~ay,~az      [m/s^2] measured acceleration in body coordinates
+    %       ~h               [m]     estimated altitude from altimeter NED, POSITIVE UP!
+    %       ~pxdot           [m/s]   x velocity from GPS (NED coordinates)
+    %       ~pydot           [m/s]   y velocity from GPS (NED coordinates)
+    %       ~hdot            [m/s]   altitude rate from altimeter (NED coordinates)
+    %
+    % U   - controls  = [pt,rl,th,ya,bat]
+    %       pt  [-0.89..0.89]  [rad]   commanded pitch
+    %       rl  [-0.89..0.89]  [rad]   commanded roll
+    %       th  [0..1]         unitless commanded throttle
+    %       ya  [-4.4,4.4]     [rad/s] commanded yaw velocity
+    %       bat [9..12]        [Volts] battery voltage
+    %
+    
+    methods (Access = public)
+        function obj = PelicanForWindTesting(objparams)
+            % simply calls the Pelican constructor
+            %
+            obj=obj@Pelican(objparams);            
+        end
+    end
+        
+    methods (Sealed,Access=protected)
+        function obj = update(obj,U)
+            % updates the state of the platform and of its components
+            % This is a clone of the Pelican update method with additional harness to write out data logs 
+            %
+            % In turns this
+            % updates turbulence model
+            % updates the state of the platform applying controls
+            % updates local part of gps model
+            % updates ahars noise model
+            % updates the graphics
+            %
+            % Note:
+            %  this method is called automatically by the step() of the Steppable parent
+            %  class and should not be called directly.
+            %
+            global state;
+            
+            if(obj.valid)
+                
+                % do scaling of inputs
+                US = obj.scaleControls(U);
+                
+                if (size(U,1)~=5)
+                    error('a 5 element column vector [-2048..2048;-2048..2048;0..4096;-2048..2048;9..12] is expected as input ');
+                end
+                
+                %wind and turbulence this closely mimic the Simulink example "Lightweight Airplane Design"
+                % asbSkyHogg/Environment/WindModels
+                state.i=state.i+1;
+                state.simin(state.i,:)=[state.t,m2ft(-obj.X(3)),m2ft(norm(obj.X(7:9))),obj.X(4),obj.X(5),obj.X(6)];
+                
+                meanWind = state.environment.wind.getLinear(obj.X);
+                
+                obj.aerodynamicTurbulence.step(obj.X);
+                turbWind = obj.aerodynamicTurbulence.getLinear(obj.X);
+                
+                accNoise = obj.dynNoise.*[randn(state.rStreams{obj.prngIds(1)},1,1);
+                                          randn(state.rStreams{obj.prngIds(2)},1,1);
+                                          randn(state.rStreams{obj.prngIds(3)},1,1);
+                                          randn(state.rStreams{obj.prngIds(4)},1,1);
+                                          randn(state.rStreams{obj.prngIds(5)},1,1);
+                                          randn(state.rStreams{obj.prngIds(6)},1,1)];
+            
+                % dynamics
+                [obj.X obj.a] = ruku2('pelicanODE', obj.X, [US;meanWind + turbWind; obj.MASS; accNoise], obj.dt);
+                
+                
+                if(obj.thisStateIsWithinLimits(obj.X) && ~obj.inCollision())
+                    
+                    % AHARS
+                    obj.ahars.step([obj.X;obj.a]);
+                    
+                    estimatedAHA = obj.ahars.getMeasurement([obj.X;obj.a]);
+                    
+                    % GPS
+                    obj.gpsreceiver.step(obj.X);
+                    
+                    estimatedPosNED = obj.gpsreceiver.getMeasurement(obj.X);
+                    
+                    %return values
+                    obj.eX = [estimatedPosNED(1:3);estimatedAHA(1:3);zeros(3,1);...
+                        estimatedAHA(4:6);0;estimatedAHA(7:10);estimatedPosNED(4:5);estimatedAHA(11)];
+                    
+                    % graphics
+                    obj.graphics.update(obj.X);
+                    
+                    obj.valid = 1;
+                else
+                    obj.eX = nan(20,1);
+                    obj.valid=0;
+                    
+                    obj.printStateNotValidError();
+                end                 
+            end
+        end        
+    end
+end
+
