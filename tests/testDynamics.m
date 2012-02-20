d9d6ef7c22bc8206cc2335b3a047522b1127a85a
Renzo De Nardi <renzodenardi@gmail.com>
2012-02-20 15:09:52 +0000

    rotationtest at 50Hz

--- tests/testDynamics.m
+++ tests/testDynamics.m
@@ -2,28 +2,82 @@ function [ e ] = testDynamics()
 %test that the response of the dynamic model to the control input matches
 %the flight data
 
-clear all;
+%clear all;
 close all;
 
 cd('dynamics');
 
 plots = 1;
 
+
+e = 0;
+
+%e = e | compareThrustToLogs(plots);
 % run the sim using logged inputs and comparing outputs with logged data
-compareRatationToLogs(plots);
+e = e | compareRatationToLogs(plots);
 
 
 cd('..');
 
 end
 
-function e = compareRatationToLogs(plots)
 
-e = 0;
+function e = compareThrustToLogs(plots)
 
 UAVCTRL_2_SI = [-deg2rad(0.025),-deg2rad(0.025),1/4097,-deg2rad(254.760/2047)]; % conversion factors
 
-data = csvread('preliminaryRotation_allBalanced01_2011-09-02-20-47-53_synced.csv');  % flight data file
+data = csvread('thrustTestSquareVariable_Batt1_2011-08-23-17-08-26.csv');  % flight data file
+
+N = length(data);
+data = data(1:N,:);
+
+% preallocate state array
+X = zeros(N,1);
+
+% Observation variables: thrust, 
+Z = data(:,3);
+
+% Input variables pt;rl;th;ya;
+U = zeros(5,N);
+U(3,:) = (data(:,2).*UAVCTRL_2_SI(3))';
+U(5,:) = data(:,4)';
+
+% new state structure
+global state;
+
+% create simulator object
+qrsim = QRSim();
+
+% load task parameters and do housekeeping
+qrsim.init('TaskDynamicsCompareThrust');
+
+for i=1:N
+    % step simulator
+    qrsim.step(U(:,i));
+    
+    X(i,1) = state.platforms(1).getX(13);
+end
+
+% Generate new time axis
+t = (0:state.DT:(N-1)*state.DT)';
+
+e = ~(mean((X-Z).^2)<0.01);
+
+if (plots)
+    figure();
+    plot(t,X(:,1));
+    hold on;
+    plot(t,Z(:,1),'r');
+end
+
+end
+
+
+function e = compareRatationToLogs(plots)
+
+UAVCTRL_2_SI = [-deg2rad(0.025),-deg2rad(0.025),1/4097,-deg2rad(254.760/2047)]; % conversion factors
+
+data = csvread('preliminaryRotation_allBalanced01_2011-09-02-20-47-53_synced-50Hz.csv');  % flight data file
 
 N = length(data);
 data= data(1:N,:);
@@ -59,8 +113,7 @@ end
 % Generate new time axis
 t = (0:state.DT:(N-1)*state.DT)';
 
-mean((X(:,1:3)-Z(:,1:3)).^2)
-
+e = ~all(mean((X(:,1:6)-Z(:,1:6)).^2)<[0.001,0.001,0.02,0.03,0.02,0.03]);
 
 if (plots)
     figure();
