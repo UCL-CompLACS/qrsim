01ce228d727ef3f294978c10b815cf2aecee8f48
Renzo De Nardi <renzodenardi@gmail.com>
2012-04-16 22:38:13 +0100

    added complacs task

---
+++ example/mainComplacs.m
@@ -0,0 +1,51 @@
+% bare bones example of use of the QRSim() simulator object with one
+% helicopter
+
+clear all
+close all
+clc
+global state;
+
+video = 1;
+
+% only needed if using the pid controller
+clear global pid;
+
+% create simulator object
+qrsim = QRSim();
+
+% load task parameters and do housekeeping
+qrsim.init('TaskComplacs');
+
+% number of steps we run the simulation for
+N = 30000;
+
+
+% the complacs path
+wps=[ 6  0  0 14 14  8  8 16 16 19 22 22 24 24 30 30 24 24 32 32 32 40 43 46 49 49 55 49 49 63 63 57 57 63;
+     -8 -8  0  0 -8 -8  0  0 -8 -4 -8  0  0 -8 -8 -4 -4  0  0 -8  0  0 -8  0  0 -8 -8 -8  0  0 -4 -4 -8 -8;
+     -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9;
+      0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0];  
+
+
+
+wpidx = 1;
+for i=1:N,    
+    ex = state.platforms(1).getEX();
+    if((norm(ex(1:3)-wps(1:3,wpidx))<0.4) && (wpidx<size(wps,2)))
+        wpidx = wpidx+1;
+    end    
+    
+    % compute controls
+    U = quadrotorPID(ex,wps(:,wpidx)');
+    % step simulator
+    qrsim.step(U);
+    
+    % get reward
+    % qrsim.reward();
+    
+    % wait so to run in real time
+    %wait = max(0,state.DT-toc(tloop));
+    pause(0.01);
+    
+end
---
+++ sim/tasks/TaskComplacs.m
@@ -0,0 +1,126 @@
+classdef TaskComplacs<Task
+    % Simple task in which a qudrotor has to keep its starting position despite the wind.
+    % Single platform task which requires to maintain the quadrotor hovering at the 
+    % position it has when the task starts; the solution requires non constant control 
+    % since the helicopter is affected by time varying wind disturbances.
+    %
+    % KeepSpot methods:
+    %   init()   - loads and returns all the parameters for the various simulator objects
+    %   reward() - returns the instantateous reward for this task
+    %
+    % 
+    % GENERAL NOTES:
+    % - if the on flag is zero, the NOISELESS version of the object is loaded instead
+    % - the step dt MUST be always specified eve if on=0
+    %
+    properties (Constant)
+        PENALTY = 1000;
+    end    
+        
+    methods (Sealed,Access=public)
+        
+        function taskparams=init(obj)
+            % loads and returns all the parameters for the various simulator objects
+            %
+            % Example:
+            %   params = obj.init();
+            %          params - all the task parameters
+            %
+            
+            % Simulator step time in second this should not be changed...
+            taskparams.DT = 0.02;
+            
+            taskparams.seed = 0; %set to zero to have a seed that depends on the system time
+            
+            %%%%% visualization %%%%%
+            % 3D display parameters
+            taskparams.display3d.on = 1;
+            taskparams.display3d.width = 1000;
+            taskparams.display3d.height = 600;            
+            
+            %%%%% environment %%%%%
+            % these need to follow the conventions of axis(), they are in m, Z down
+            % note that the lowest Z limit is the refence for the computation of wind shear and turbulence effects
+            taskparams.environment.area.limits = [-15 80 -15 15 -20 0];
+            taskparams.environment.area.type = 'BoxArea';
+            
+            % originutmcoords is the location of the RVC (our usual flying site)
+            % generally when this is changed gpsspacesegment.orbitfile and 
+            % gpsspacesegment.svs need to be changed
+            [E N zone h] = llaToUtm([51.71190;-0.21052;0]);
+            taskparams.environment.area.originutmcoords.E = E;
+            taskparams.environment.area.originutmcoords.N = N;
+            taskparams.environment.area.originutmcoords.h = h;
+            taskparams.environment.area.originutmcoords.zone = zone;
+            taskparams.environment.area.graphics.type = 'AreaGraphics';
+            taskparams.environment.area.graphics.backgroundimage = 'ucl-rvc-zoom.tif';
+            
+            % GPS
+            % The space segment of the gps system
+            taskparams.environment.gpsspacesegment.on = 0; % if off the gps returns the noiseless position
+            taskparams.environment.gpsspacesegment.dt = 0.2;
+            % real satellite orbits from NASA JPL
+            taskparams.environment.gpsspacesegment.orbitfile = 'ngs15992_16to17.sp3';
+            % simulation start in GPS time, this needs to agree with the sp3 file above, 
+            % alternatively it can be set to 0 to have a random initialization
+            %taskparams.environment.gpsspacesegment.tStart = Orbits.parseTime(2010,8,31,16,0,0); 
+            taskparams.environment.gpsspacesegment.tStart = 0;             
+            % id number of visible satellites, the one below are from a typical flight day at RVC
+            % these need to match the contents of gpsspacesegment.orbitfile
+            taskparams.environment.gpsspacesegment.svs = [3,5,6,7,13,16,18,19,20,22,24,29,31];
+            % the following model is from [2]
+            %taskparams.environment.gpsspacesegment.type = 'GPSSpaceSegmentGM';
+            %taskparams.environment.gpsspacesegment.PR_BETA = 2000;     % process time constant
+            %taskparams.environment.gpsspacesegment.PR_SIGMA = 0.1746;  % process standard deviation
+            % the following model was instead designed to match measurements of real
+            % data, it appears more relistic than the above
+            taskparams.environment.gpsspacesegment.type = 'GPSSpaceSegmentGM2';            
+            taskparams.environment.gpsspacesegment.PR_BETA2 = 4;       % process time constant
+            taskparams.environment.gpsspacesegment.PR_BETA1 =  1.005;  % process time constant   
+            taskparams.environment.gpsspacesegment.PR_SIGMA = 0.003;   % process standard deviation            
+            
+            % Wind
+            % i.e. a steady omogeneous wind with a direction and magnitude
+            % this is common to all helicopters
+            taskparams.environment.wind.on = 0;
+            taskparams.environment.wind.type = 'WindConstMean';
+            taskparams.environment.wind.direction = degsToRads(45); %mean wind direction, rad clockwise from north set to [] to initialise it randomly
+            taskparams.environment.wind.W6 = 0.5;  % velocity at 6m from ground in m/s
+            
+            %%%%% platforms %%%%%
+            % Configuration and initial state for each of the platforms
+            taskparams.platforms(1).configfile = 'pelican_config_complacs';
+            taskparams.platforms(1).X = [6;-8;-9;0;0;0];
+        end
+        
+        function r=reward(obj) 
+            % returns the instantateous reward for this task
+            %
+            % Example:
+            %   r = obj.reward();
+            %          r - the reward
+            %
+            global state;
+            
+            if(state.platforms(1).valid)
+                e = state.platforms(1).X(1:12);
+                e = e(1:3)-state.platforms(1).params.X(1:3);
+                r = - e' * e; 
+            else
+                % returning a large penalty in case the state is not valid
+                % i.e. the helicopter is out of the area, there was a
+                % collision or the helicopter has crashed 
+                r = - obj.PENALTY;
+            end
+                
+        end
+    end
+    
+end
+
+
+
+% [1] J. Rankin, "An error model for sensor simulation GPS and differential GPS," IEEE
+%     Position Location and Navigation Symposium, 1994, pp.260-266.
+% [2] Carlson, Justin, "Mapping Large, Urban Environments with GPS-Aided SLAM" (2010).
+%     Dissertations. Paper 44.
