4d450bf88cff72c602149b55de1219392877803a
Renzo De Nardi <renzodenardi@gmail.com>
2012-09-27 21:05:34 +0100

    fixed a few naming bugs

--- scenarios/plume/main_plume.m
+++ scenarios/plume/main_plume.m
@@ -6,7 +6,7 @@
 % The plume follows a known model but with unknown parameter values. 
 % The objective is to provide a smoke concentration estimate cT at some prespecified time T
 
-clear all
+%clear all
 close all
 
 % include simulator
@@ -28,23 +28,23 @@ state = qrsim.init('TaskPlumeSingleSourceGaussian');
 
 % create a 2 x helicopters matrix of control inputs
 % column i will contain the 2D NED velocity [vx;vy] in m/s for helicopter i
-U = zeros(2,qrsim.task.Nc);
+U = zeros(2,state.task.numUAVs);
 tstart = tic;
 
 % run the scenario and at every timestep generate a control
 % input for each of the helicopters
-for i=1:qrsim.task.durationInSteps,
+for i=1:state.task.durationInSteps,
     tloop=tic;
     
     % a basic policy in which the helicopter(s) moves around 
     % at the max velocity in rand directions 
-    for j=1:qrsim.task.numUAVs,        
+    for j=1:state.task.numUAVs,        
 	
-	% random velocity direction
-	u = rand(2,1);        
+        % random velocity direction
+        u = rand(2,1);        
 
         % scale by the max allowed velocity
-        U(:,j) = qrsim.task.velPIDs{j}.maxv*(u/norm(u));
+        U(:,j) = state.task.velPIDs{j}.maxv*(u/norm(u));
     end
     
     % step simulator
@@ -56,11 +56,19 @@ for i=1:qrsim.task.durationInSteps,
     pause(wait);
 end
 
-% get final reward
+% query at what locations we need to make predictions of concentration
+points = state.task.getLocations();
+
+samples = zeros(1,size(points,2));
+
+% set the samples so that the task can compute a reward
+state.task.setSamples(samples);
+
+% get final reward, this works only after the samples have been set.
 % reminder: a large negative final reward (-1000) is returned in case of
 % collisions or in case of any uav going outside the flight area
 fprintf('final reward: %f\n',qrsim.reward());
 
 elapsed = toc(tstart);
 
-fprintf('running %d times real time\n',(qrsim.task.durationInSteps*state.DT)/elapsed);
+fprintf('running %d times real time\n',(state.task.durationInSteps*state.DT)/elapsed);
