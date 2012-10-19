e261db28dbd4c966dfeb0581968640f497e45964
Renzo De Nardi <renzodenardi@gmail.com>
2012-10-19 19:16:42 +0100

    comments

--- scenarios/searchrescue/main_searchrescue.m
+++ scenarios/searchrescue/main_searchrescue.m
@@ -1,17 +1,15 @@
 % bare bones example of use of the QRSim() simulator
-% with one of the the cats-mouse scenario
+% with one of the search-and-rescue scenario
 %
-% as a quick remainder, in this task which three quadrotors (cats) have to catch another
-% quadrotor (mouse) AT THE END of the allotted time for the task.
-% In other words we have only a final cost equal to the sum of the
-% squared distances of the cats to the mouse. A large negative reward
-% is returned if any of the helicopters goes outside of the flying area.
-% For simplicity all quadrotors are supposed to fly at the same altitude.
-% The initial position of the quadrotors is defined randomly
-% (within reason) around the mouse; the mouse moves at a constant (max) speed
-% and uses a predefined control law which pays more heed to cats that are close by.
+% as a quick remainder, in this task targets (people) are lost/injured on 
+% the ground in a landscape and need to be located and rescued. 
+% A helicopter agent is equipped with a camera/classification module
+% for predicting the position of targets in its field of vision, but the quality of predictions
+% depend upon the geometry between helicopter and ground (e.g. the distance). Rather
+% than raw images the camera module provides higher-level data in the form of likelihood
+% ratios of the current image conditioned on the presence or absence of a target.
 
-clear all
+%clear all
 close all
 
 % include simulator
@@ -22,52 +20,43 @@ addpath(['..',filesep,'..',filesep,'controllers']);
 qrsim = QRSim();
 
 % load task parameters and do housekeeping
-%state = qrsim.init('TaskCatsMouseNoiseless');
-%state = qrsim.init('TaskCatsMouseNoisy');
-state = qrsim.init('TaskCatsMouseNoisyAndWindy');
+state = qrsim.init('TaskSearchRescueSingleNoiseless');
+%state = qrsim.init('TaskSearchRescueSingleNoisy');
+%state = qrsim.init('TaskSearchRescueMultipleNoiseless');
+%state = qrsim.init('TaskSearchRescueMultipleNoisyAndWindy');
 
-% remainder:
-% platforms with id 1..qrsim.task.Nc are cats
-% the last platform (i.e. with id qrsim.task.Nc+1) is the mouse
 
-% create a 2 x cats matrix of control inputs
-% column i will contain the 2D NED velocity [vx;vy] in m/s for cat i
-U = zeros(2,qrsim.task.Nc);
+% create a 2 x helicopters matrix of control inputs
+% column i will contain the 2D NED velocity [vx;vy] in m/s for helicopter i
+U = zeros(2,state.task.numUAVs);
 tstart = tic;
 
 % run the scenario and at every timestep generate a control
-% input for each of the cats
-for i=1:qrsim.task.durationInSteps,
+% input for each of the uavs
+u = zeros(2,state.task.numUAVs);
+for i=1:state.task.durationInSteps,
     tloop=tic;
     
-    % get the mouse position (note id qrsim.task.Nc+1)
-    mousePos = state.platforms{qrsim.task.Nc+1}.getEX(1:2);
+    % a basic policy in which the helicopter(s) moves around
+    % at the max velocity changing direction every once in a while
     
-    % a quick and easy way of computing velocity controls for each cat (which almost never works); 
-    % REPLACE IT with your control/learning algorithm.
-    % (Note that for simplicity this control law tries to catch the mouse
-    % as soon as possible and not simply at the end of the allotted time.)
-    for j=1:qrsim.task.Nc,
-        % vector to the mouse
-        u = mousePos - state.platforms{j}.getEX(1:2);
-        
-        % if far away add a weighted velocity to "predict" where the mouse will be
-        u = u  + (norm(u)/2)*state.platforms{qrsim.task.Nc+1}.getEX(18:19);
-        
-        % keep away from other cats if closer than 2*collisionDistance
-        for k = 1:qrsim.task.Nc,
-            d = state.platforms{j}.getEX(1:2) - state.platforms{k}.getEX(1:2);
-            if((k~=j)&&(norm(d)<2*state.platforms{j}.getCollisionDistance()))
-                u = u + (1/(norm(d)-state.platforms{j}.getCollisionDistance()))*(d/norm(d));
-            end
+    if(rem(i-1,10)==0)
+        for j=1:state.task.numUAVs,            
+            % random velocity direction
+            u(:,j) = rand(2,1)-[0.5;0.5];
+            % scale by the max allowed velocity
+            U(:,j) = state.task.velPIDs{j}.maxv*(u(:,j)/norm(u(:,j)));
         end
-        
-        % scale by the max allowed velocity
-        U(:,j) = qrsim.task.velPIDs{j}.maxv*(u/norm(u));
     end
     
     % step simulator
     qrsim.step(U);
+    
+    % get plume measurement
+    for j=1:state.task.numUAVs,
+        state.platforms{j}.getCameraOutput();
+    end
+    
     
     % wait so to run in real time
     % this can be commented out obviously
