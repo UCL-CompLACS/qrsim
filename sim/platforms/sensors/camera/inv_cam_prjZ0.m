2c75dcc3066774341536ad750ae62291c8a3544c
Renzo De Nardi <renzodenardi@gmail.com>
2012-11-09 22:59:53 +0000

    fixed bugs

--- sim/platforms/sensors/camera/inv_cam_prjZ0.m
+++ sim/platforms/sensors/camera/inv_cam_prjZ0.m
@@ -1,12 +1,11 @@
-function pt = inv_cam_prjZ0(tp,Rp,uv,focal_length,principal_point, R)
-
+function pt = inv_cam_prjZ0(tp,Rp,uv,principal_point,focal_length, R)
 % bring the chosen point uv to world coords
-p = Rp'*R'*((uv-principal_point)./focal_length) + tp;
+p = Rp'*R'*[((uv-principal_point)./focal_length);1] + tp;
 
 % compute intersection of ray t-p and
 % the ground plane z=0
 z=0;
-x=(p(1)-tp(1))*((z-tp(3))/(p(3)-tp(3)))+t(1);
-y=(p(2)-tp(2))*((z-tp(3))/(p(3)-tp(3)))+t(2);
+x=(p(1)-tp(1))*((z-tp(3))/(p(3)-tp(3)))+tp(1);
+y=(p(2)-tp(2))*((z-tp(3))/(p(3)-tp(3)))+tp(2);
 
 pt=[x;y;z];
