d6d84939433fdef3d4eeb0d307792eefa21dce0e
Renzo De Nardi <renzodenardi@gmail.com>
2013-01-31 13:55:49 +0000

    removed noise in the platform dynamics

--- scenarios/searchrescue/pelican_with_camera_noiseless_config.m
+++ scenarios/searchrescue/pelican_with_camera_noiseless_config.m
@@ -36,7 +36,7 @@ c.stateLimits =[params.environment.area.limits(1:2);params.environment.area.limi
     -3,3;-3,3;-3,3];                          % rotational velocity limits
     
 c.collisionDistance = 2;                      % two platforms colser than this distance are deemed in collision 
-c.dynNoise = [0.2;0.2;0.2;0.2;0.2;0.2];
+c.dynNoise = [0;0;0;0;0;0];
 
 % GPS Receiver
 c.sensors.gpsreceiver.on = 0;                 % if off the gps returns the noiseless position
